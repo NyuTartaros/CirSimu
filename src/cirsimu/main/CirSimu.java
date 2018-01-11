ead3ecac82bbde7ca0410c0c9d2feaf1f90df8ab
NyuTartaros <niuyunfei123@live.cn>
2018-01-11 19:06:49 +0800

    update

--- src/cirsimu/main/CirSimu.java
+++ src/cirsimu/main/CirSimu.java
@@ -1,10 +1,12 @@
 package cirsimu.main;
 
-public class CirSimu {
+import cirsimu.ui.MainWindow;
 
+public class CirSimu {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MainWindow mainWindow = new MainWindow();
 	}
 
 }
--- src/cirsimu/ui/MainWindow.java
+++ src/cirsimu/ui/MainWindow.java
@@ -3,5 +3,11 @@ package cirsimu.ui;
 import javax.swing.JFrame;
 
 public class MainWindow extends JFrame {
+	public MainWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("\u7535\u8DEF\u7F16\u8F91\u4EFF\u771F");
+		setLocationRelativeTo(null);
+		setSize(1000,800);
+	}
 
 }
